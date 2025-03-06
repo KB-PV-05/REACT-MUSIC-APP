50280469568117d627e4cc918b502984af3b63ab
K.B. Parvathy Vidhya <kbparvathyvidhya@gmail.com>
2025-03-06 19:24:15 +0530

    Added JSON Server setup for mock API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Overview from "./pages/Overview";
 import CollectionDetails from "./pages/CollectionDetails";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Overview />} />
-        <Route path="/collection/:id" element={<CollectionDetails />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Overview />} />
+      <Route path="/collection/:id" element={<CollectionDetails />} />
+    </Routes>
   );
 }
 
